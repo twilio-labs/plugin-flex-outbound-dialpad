df288d7445a687e0fb787dd0609bedc0b342f4be
Jared Hunter <jhunter@twilio.com>
2019-05-29 15:17:58 -0500

    Updating notifications additions to only be added if the havent already

--- src/notifications/CustomNotifications.js
+++ src/notifications/CustomNotifications.js
@@ -2,17 +2,23 @@ import { Notifications, NotificationType, Manager } from "@twilio/flex-ui";
 
 const manager = Manager.getInstance();
 
-manager.strings.websocketError = "Unable to open websocket to backend: {{url}}";
-manager.strings.backendError = "Could not complete operation: {{message}}";
+// To avoid collision with other plugin using same notification
+if (!manager.strings.websocketError) {
+  manager.strings.websocketError =
+    "Unable to open websocket to backend: {{url}}";
+  Notifications.registerNotification({
+    id: "WebsocketError",
+    content: "websocketError",
+    type: NotificationType.error
+  });
+}
 
-Notifications.registerNotification({
-  id: "WebsocketError",
-  content: "websocketError",
-  type: NotificationType.error
-});
+if (!manager.strings.backendError) {
+  manager.strings.backendError = "Could not complete operation: {{message}}";
 
-Notifications.registerNotification({
-  id: "BackendError",
-  content: "backendError",
-  type: NotificationType.error
-});
+  Notifications.registerNotification({
+    id: "BackendError",
+    content: "backendError",
+    type: NotificationType.error
+  });
+}
